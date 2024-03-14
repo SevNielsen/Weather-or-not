06d8d3ad2c11f193b75778733e9e4b36a2d2e5f5
Seth <seth897476@gmail.com>
2024-03-13 22:56:47 -0700

    Added alerts for when users successfully sign up. also added alerts for when users don't successfully sign up

--- Weather_App/website/__init__.py
+++ Weather_App/website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy() # object to use whenever you want to add new person to database, create new user, etc
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Secret_Key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
