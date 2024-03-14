468bba59da15359ce421be7dd04ddc1408469115
Seth <seth897476@gmail.com>
2024-03-14 11:52:29 -0700

    Working on the database now

--- Weather_App/website/models.py
+++ Weather_App/website/models.py
@@ -2,5 +2,9 @@ from . import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.)
+    password = db.Column()
+    email = db.Column()
+    city = db.Column()
     
