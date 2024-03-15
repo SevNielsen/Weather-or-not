5e52788ac856477b669c345da95d2a3214a1ecca
Seth <seth897476@gmail.com>
2024-03-14 17:23:24 -0700

    Users can now successfully put in their information in the sign up page and create an account.

--- Weather_App/website/models.py
+++ Weather_App/website/models.py
@@ -1,10 +1,35 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
-class User(db.Model, UserMixin):
+class Member(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.)
-    password = db.Column()
-    email = db.Column()
-    city = db.Column()
-    
+    username = db.Column(db.String(50), unique = True)
+    password = db.Column(db.String(50), unique = True)
+    email = db.Column(db.String(50), unique = True)
+    city = db.Column(db.String(50))
+    notifications = db.Column(db.Boolean(), default = False)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    date = db.Column(db.DateTime(timezone=True), default =func.now())
+
+#class Guest(db.Model, UserMixin):
+ #   id = db.Column(db.Integer, primary_key = True)
+  #  username = db.Column(db.String(50), unique = True)
+  #  password = db.Column(db.String(50), unique = True)
+  #  email = db.Column(db.String(50), unique = True)
+  #  city = db.Column(db.String(50))
+  #  notifications = db.Column(db.Boolean(), default = False)
+  #  first_name = db.Column(db.String(50))
+  #  last_name = db.column(db.String(50))
+
+#class Admin(db.Model, UserMixin):
+   # id = db.Column(db.Integer, primary_key = True)
+   # username = db.Column(db.String(50), unique = True)
+   # password = db.Column(db.String(50), unique = True)
+   # email = db.Column(db.String(50), unique = True)
+   # city = db.Column(db.String(50))
+   # notifications = db.Column(db.Boolean(), default = False)
+   # first_name = db.Column(db.String(50))
+   # last_name = db.column(db.String(50))
+
