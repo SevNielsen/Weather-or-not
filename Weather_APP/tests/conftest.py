d816e64a424b6972bd8c52756652e5fe80a561fe
SevNielsen <102651879+SevNielsen@users.noreply.github.com>
2024-03-29 16:37:05 -0700

    added coverage file. Working on .yml, testing, and automation.

--- Weather_APP/tests/conftest.py
+++ Weather_APP/tests/conftest.py
@@ -7,7 +7,12 @@ def app():
     app = create_app("sqlite://")
 
     with app.app_context():
-        db.create_al_all()
+        db.create_all()
 
     yield app 
+    
+@pytest.fixture()
+def client(app):
+    
+    return app.test_client()
     
