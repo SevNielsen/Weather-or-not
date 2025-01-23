ffa789fc14a819d76af21bfcca9fb5cdc9a74cec
SevNielsen <102651879+SevNielsen@users.noreply.github.com>
2025-01-23 19:03:19 +0700

    Updated the welcome/login and signup pages, still need to incorporate better functionality.

--- Weather_APP/website/__init__.py
+++ Weather_APP/website/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = 'auth.home'
     login_manager.init_app(app)
 
     @login_manager.user_loader
